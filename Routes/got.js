cd8b113e95e553cabf8f386e3532ecd486ad63cc
Thomas <malifarge.thomas9@gmail.com>
2022-08-08 17:46:27 +0200

    got/url

--- Routes/got.js
+++ Routes/got.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+const axios = require("axios")
 
 const characters = require('../got.json')
 
 app.get('/json', (req, res) =>{
    
     res.json(characters)
+})
+
+app.get('/url', async (req, res) =>{
+    const response = await axios.get("https://thronesapi.com/api/v2/Characters")
+	res.json(response.data)
 })
 
 module.exports = app
