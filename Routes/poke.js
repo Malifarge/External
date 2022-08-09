981eb4bba3a40d8f0f0c4c6f2f26350e6996dca2
Thomas <malifarge.thomas9@gmail.com>
2022-08-09 10:16:59 +0200

    let limit & 404

--- Routes/poke.js
+++ Routes/poke.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const app = express()
 const axios = require("axios")
+let limit = 151
 
 app.get('/list', async (req, res)=>{
-    const response = await axios('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    const response = await axios(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`)
     res.json(response.data.results)
 })
 
 app.get('/:id', async (req,res)=> {
     const id = req.params.id
-    const response = await axios (` https://pokeapi.co/api/v2/pokemon/${id}`)
-    res.json(response.data)
+    if (Number (id) <=905){
+        const response = await axios (` https://pokeapi.co/api/v2/pokemon/${id}`)
+        res.json(response.data)
+    }else {
+        res.status(404).send("Not Found")
+    }
 })
 
 module.exports = app
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const pokeRoutes = require("./Routes/poke")
 app.use("/game-of-thrones",gotRoutes)
 app.use("/pokemon",pokeRoutes)
 
+app.get("/", (req,res)=>{
+    res.send('Got/poke API')
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
